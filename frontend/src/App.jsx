1e71d67695168ea9d6c53635137e90ee78beb335
Harold <harold@iscompsystems.com>
2024-03-29 16:39:05 -0700

    Beginning Google Maps AutoComplete Address Matching Feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,11 @@
-import { useForm } from "react-hook-form"
-
+import React from "react";
+import Form from "./components/form.jsx";
 
 export default function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm()
-
-
-  const onSubmit = (data) => console.log(data)
-
-
-  console.log(watch("example")) // watch input value by passing the name of it
-
-
   return (
-    /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)} className="card">
-      {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
-
-
-      {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
-
-
-      <input type="submit" />
-    </form>
+      <div>
+          <div>Welcome To The Form</div>
+          <Form/>
+      </div>
   )
 }
---
+++ frontend/src/components/form.jsx
@@ -0,0 +1,12 @@
+import AutoComplete from "./autoComplete.jsx";
+import React, {useEffect, useRef} from "react";
+
+const Form = () => {
+    return (
+        <div>
+            <div>Yo Im Da Outta Form</div>
+          <AutoComplete/>
+        </div>
+    );
+};
+export default Form;
